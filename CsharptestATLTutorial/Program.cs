04b00cce39d108a84cc7e79c4dc26b4127aabf38
chernic <iamchernic@gmail.com>
2018-04-23 15:20:59 +0800

    Add CsharptestATLTutorial

---
+++ CsharptestATLTutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsharptestATLTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ATLTutorial2008Lib.HelloWorld hw = new ATLTutorial2008Lib.HelloWorld();
+            System.Diagnostics.Debug.WriteLine("===>" + hw.SayHello("abc"));
+            Console.ReadKey();
+        }
+    }
+}
